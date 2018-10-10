c1da61e75477980071b33083de61f45e39fa4755
ash16 <ashnaaggarwal@gmail.com>
2018-10-10 18:12:06 -0400

    Failed divide

--- spec/unit/divider.spec.js
+++ spec/unit/divider.spec.js
@@ -5,4 +5,12 @@ describe("divider tests ", function() {
     var _numberB;
 
 
+    it("should return 2", function(){
+        _numberA = 1
+        _numberB = 2
+
+        var result = divider.divide(_numberA, _numberB);
+
+        expect(result).toEqual(2);
+    })
 });
