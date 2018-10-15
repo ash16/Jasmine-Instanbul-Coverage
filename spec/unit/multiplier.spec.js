aee335bfa8de6b9e1e3ceb6fb5d8cde7234c4250
ash16 <ashnaaggarwal@gmail.com>
2018-10-15 18:31:23 -0400

    Fixed unit tests

--- spec/unit/multiplier.spec.js
+++ spec/unit/multiplier.spec.js
@@ -9,7 +9,7 @@ describe("multiplier tests ", function() {
         _numberB = 2;
         var result = multiplier.multiple(_numberA, _numberB);
 
-        expect(result).toEqual(3);
+        expect(result).toEqual(4);
     });
 
     it("should return undefined for numberA equals undefined", function() {
